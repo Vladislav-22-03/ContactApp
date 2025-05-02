f2f174f9c390abed413565b69b60668d39b58bd7
Pirojok_ot_Lovet <145265021+Vladislav-22-03@users.noreply.github.com>
2025-05-02 15:59:18 +0300

    Final

--- app/src/main/java/com/example/contactapp/ContactService.kt
+++ app/src/main/java/com/example/contactapp/ContactService.kt
@@ -3,14 +3,12 @@ package com.example.contactapp
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import com.example.contactapp.utils.ContactUtils  // Убедитесь, что утилита для контактов подключена
+import com.example.contactapp.utils.ContactUtils
 
 class ContactService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // Здесь можно вызвать метод, который удалит дубликаты
         val result = ContactUtils.deleteDuplicateContacts(this)
 
-        // Отправим результат обратно
         val resultIntent = Intent("com.example.contactapp.DELETE_STATUS")
         resultIntent.putExtra("result", result)
         sendBroadcast(resultIntent)
